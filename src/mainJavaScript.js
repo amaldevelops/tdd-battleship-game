c66194bda191cc67ae54caa2659bd5568498c33f
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-07-26 20:20:53 +1000

    Completed Ship Class with Jest tests Passed

--- src/mainJavaScript.js
+++ src/mainJavaScript.js
@@ -4,19 +4,40 @@
 //Following will import the CSS file used to style the project
 import "./mainStyleSheet.css";
 
+class ship {
+  constructor(shipLength) {
+    this.shipLength = shipLength;
+    this.numberOfHits=0;
+    this.shipSunk = false;
+  }
 
-class ship
-{
-    constructor(shipLength,numberOfHits,shipSunk)
-    {
-        this.shipInfoObject={shipLength:this.shipLength,
-            numberOfHits:this.numberOfHits,
-            shipSunk:this.shipSunk,
-
+  hit()
+  {
+    if (!this.shipSunk)
+        {
+            this.numberOfHits += 1;
+        }
+    if (this.numberOfHits>=this.shipLength)
+        {
+            this.shipSunk=true;
         }
 
-    }
+  }
+
+  isSunk()
+{
+    return this.shipSunk;
 }
 
 
-export {ship};
+  shipStatus() {
+    this.shipInfoObject = {
+      shipLength: this.shipLength,
+      numberOfHits: this.numberOfHits,
+      shipSunk: this.shipSunk,
+    };
+    return this.shipInfoObject;
+  }
+}
+
+export { ship };
--- src/testSuite.test.js
+++ src/testSuite.test.js
@@ -1,12 +1,39 @@
 import {ship} from './mainJavaScript';
 
-const newShip=new ship(2,0,false);
-const newShipReturnedObeject={shipLength:2,numberOfHits:0,
-    shipSunk:true}
+const newShip=new ship(2);
+const newShipReturnedObject={shipLength:2,numberOfHits:0,
+    shipSunk:false};
+
+const newShipDamage={shipLength:2,numberOfHits:1,
+        shipSunk:false};
+
+const newShipSunk={shipLength:2,numberOfHits:2,
+            shipSunk:true};
 
 
-
-test('This test will check for Ship Class',()=>{
-expect(newShip()).toBe(newShipReturnedObeject);
+test('This test will check for Ship Status when first initialized',()=>{
+expect(newShip.shipStatus()).toStrictEqual(newShipReturnedObject);
 
 });
+
+test('This test will check whether ship is NOT Sunk',()=>{
+    expect(newShip.isSunk()).toStrictEqual(false);
+    });
+
+test('This test will check for Ships Damage after 1 hit',()=>{
+    newShip.hit();
+expect(newShip.shipStatus()).toStrictEqual(newShipDamage);
+});
+
+test('This test will check for Ships Damage after 2 hits',()=>{
+    newShip.hit();
+    newShip.hit();
+    newShip.hit();
+
+expect(newShip.shipStatus()).toStrictEqual(newShipSunk);
+});
+
+test('This test will check whether ship is Sunk',()=>{
+expect(newShip.isSunk()).toStrictEqual(true);
+});
+
