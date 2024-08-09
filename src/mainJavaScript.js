ae45be8912fc0d87bb1d0bda20f0987c73dfe4f1
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-08-09 14:23:18 +1000

    Add remaining Coding tasks to the mainJavaScript

--- src/mainJavaScript.js
+++ src/mainJavaScript.js
@@ -1,5 +1,17 @@
 //This is the main JavaScript file
 
+/*
+Remaining Tasks
+* Code for Ai player to make random choices on the Human Players game board
+* Code for game flow to hand over next turn to players
+* Code to monitor sunk ships of each player and announce winner when remaining ships == 0
+* Code to monitor Ai board in the console
+* Code to update Ai Player board with Human player clicked spots and disabling anymore clicks on the same spot
+* Code to show ai players sunk ships on the ai player board
+* Code to color the gameboard according to sunk ships(Red), active ships (Green) and clicked spot (Gray), use CSS classes to do this
+* CSS to make the game look nicer
+*/
+
 //ES Module Imports
 //Following will import the CSS file used to style the project
 import "./mainStyleSheet.css";
@@ -15,4 +27,3 @@ gameInitialization.startGame();
 
 //ES Module Exports
 export { Ship, Gameboard, Player }; //Class exports
-// export { gameBoardHuman, gameBoardAi }; //Class instance exports
